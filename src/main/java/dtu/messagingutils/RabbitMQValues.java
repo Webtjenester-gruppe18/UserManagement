607716f910c9ff483dc0d18a451b1d160d8eae51
Marcus August Christiansen <s175185@student.dtu.dk>
2020-01-21 20:04:37 +0100

    host name to rabbitmq

--- src/main/java/dtu/messagingutils/RabbitMQValues.java
+++ src/main/java/dtu/messagingutils/RabbitMQValues.java
@@ -17,6 +17,6 @@ public class RabbitMQValues {
     public static final String REPORTING_SERVICE_QUEUE_NAME = "reportingservice-queue";
     static final String REPORTING_SERVICE_ROUTING_KEY = "reporting";
 
-    public static final String HOST_NAME = "localhost";
+    public static final String HOST_NAME = "rabbitmq";
 
 }
